c060137ca4c51e39d33166d8081fee9643796b5a
Suguru Imanaga <suguru_imanaga@cysista.co.jp>
2020-05-13 17:37:22 +0900

    test

--- bin/build.sh
+++ bin/build.sh
@@ -4,4 +4,4 @@ git clone -b master https://github.com/Imasug/multi-wars2.git --depth 1
 
 mvn clean package -f multi-wars2/pom.xml
 
-cp `find . -name *.war` $HOME/deployments
+cp `find . -name *.war` /tmp/src/deployments
